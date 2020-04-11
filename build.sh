11ed773ce171efc094aaf531e67de3d83b53fc29
hawkeye116477 <hawkeye116477@gmail.com>
2020-04-11 10:59:09 +0200

    Tidy up a little and add build script.

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+XPI_FILE=s4e-revived
+
+path=$(dirname "$0")
+
+cd $path/src
+
+echo "Removing old xpi file"
+if [ -f ../"$XPI_FILE.xpi" ]; then
+    rm ../$XPI_FILE.xpi
+fi
+
+echo "Removing old xpt file"
+if [ -f ./components/status4evar.xpt ]; then
+    rm ./components/status4evar.xpt
+fi
+
+echo "Creating xpt file"
+python2 $(xdg-user-dir DOWNLOAD)/xulrunner-sdk/sdk/bin/typelib.py ./components/status4evar.idl -o ./components/status4evar.xpt -I /$(xdg-user-dir DOWNLOAD)/xulrunner-sdk/idl/
+
+echo "Creating xpi file"
+zip -r ../$XPI_FILE.xpi *
