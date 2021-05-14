80e8417c93b65ece6540915a2dd07325195430a6
hawkeye116477 <hawkeye116477@gmail.com>
2021-05-14 11:06:46 +0200

    Fix and cleanup install.rdf

--- build.sh
+++ build.sh
@@ -2,22 +2,55 @@
 
 XPI_FILE=s4e-revived
 
-path=$(dirname "$0")
+path=$(dirname "$(realpath -s "$0")")
+temp_path="$path"/src_temp
 
-cd $path/src
+cd "$path"/src || exit
 
-echo "Removing old xpi file"
-if [ -f ../"$XPI_FILE.xpi" ]; then
-    rm ../$XPI_FILE.xpi
-fi
+mkdir "$temp_path"
+cp -r "$path"/src/* "$temp_path"/
 
-echo "Removing old xpt file"
-if [ -f ./components/status4evar.xpt ]; then
-    rm ./components/status4evar.xpt
-fi
+cd "$temp_path" || exit
+
+cp ../LICENSE ./
+
+mapfile -t locales < <(find "$temp_path"/chrome/locale -maxdepth 1 -exec basename {} \; | sed s/en$// | sed s/LICENSE$// | sed s/LICENSE.in$// | sed s/locale$// | sed -r '/^\s*$/d' | sort -u)
+
+for locale in "${locales[@]}"; do
+extName=$(grep 'name=' "$temp_path"/chrome/locale/"$locale"/meta.properties | cut -d'=' -f2)
+extDesc=$(grep 'description=' "$temp_path"/chrome/locale/"$locale"/meta.properties | cut -d'=' -f2)
+extTrans=$(grep 'translator=' "$temp_path"/chrome/locale/"$locale"/meta.properties | cut -d'=' -f2)
+extContr=$(grep 'em:contributor=' "$temp_path"/install.rdf | cut -d'=' -f2 | sed -e 's/^"//' -e 's/"$//')
+localized+=$(cat <<EOF
+
+    <em:localized>
+      <Description
+        em:locale="$locale"
+        em:name="$extName"
+        em:description="$extDesc"
+        em:translator="$extTrans"
+        em:contributor="$extContr"/>
+    </em:localized>
+\l
+EOF
+)
+done
+
+localized=$(echo "${localized}" | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/\$/\\$/g' | sed '/^$/d')
+sed -i "s|_localized_|$localized|" "$temp_path"/install.rdf
 
 echo "Creating xpt file"
-python2 $(xdg-user-dir DOWNLOAD)/firefox-sdk/sdk/bin/typelib.py ./components/status4evar.idl -o ./components/status4evar.xpt -I $(xdg-user-dir DOWNLOAD)/firefox-sdk/idl/
+python2 "$(xdg-user-dir DOWNLOAD)"/firefox-sdk/sdk/bin/typelib.py ./components/status4evar.idl -o ./components/status4evar.xpt -I "$(xdg-user-dir DOWNLOAD)"/firefox-sdk/idl/
+
+
+echo "Removing old xpi file"
+if [ -f "$path/$XPI_FILE.xpi" ]; then
+    rm "$path/$XPI_FILE.xpi"
+fi
 
 echo "Creating xpi file"
-zip -r ../$XPI_FILE.xpi *
+zip -r $XPI_FILE.xpi ./*
+
+cd "$path" || exit
+mv "$temp_path/$XPI_FILE".xpi "$path"/
+rm -rf "$temp_path"
